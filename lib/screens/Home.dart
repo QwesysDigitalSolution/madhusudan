026944d7fdef076d3b30a36a3c1c6d97f5399d3c
QwesysDigitalSolution <qwesyssolutions@gmail.com>
2020-02-04 18:25:47 +0530

    KRS

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:madhusudan/common/Constants.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,8 +9,131 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    double widt = MediaQuery.of(context).size.width;
     return Container(
-      child: Text("Home"),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: widt * 0.05,
+            ),
+            child: Image.asset(
+              "images/logo.png",
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+            padding: EdgeInsets.symmetric(vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 0.5,color: Colors.grey),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              /*boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 20.0, // soften the shadow
+                  //spreadRadius: 1.0, //extend the shadow
+                  offset: Offset(
+                    10.0, // Move to right 10  horizontally
+                    10.0, // Move to bottom 10 Vertically
+                  ),
+                ),
+              ],*/
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  "Upload Order Photo",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                Image.asset("images/camera.png"),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  //margin: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                  width: widt * 0.44,
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 0.5,color: Colors.grey),
+                    /*boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 20.0, // soften the shadow
+                    //spreadRadius: 1.0, //extend the shadow
+                    offset: Offset(
+                      10.0, // Move to right 10  horizontally
+                      10.0, // Move to bottom 10 Vertically
+                    ),
+                  ),
+                ],*/
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Image.asset(
+                        "images/box.png",
+                        height: 150,
+                      ),
+                      Text(
+                        "Box",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  //margin: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                  width: widt * 0.44,
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 0.5,color: Colors.grey),
+                    /*boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 20.0, // soften the shadow
+                    //spreadRadius: 1.0, //extend the shadow
+                    offset: Offset(
+                      10.0, // Move to right 10  horizontally
+                      10.0, // Move to bottom 10 Vertically
+                    ),
+                  ),
+                ],*/
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Image.asset(
+                        "images/sareeicon.jpg",
+                        height: 150,
+                      ),
+                      Text(
+                        "Loose",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
