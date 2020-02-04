108dfda7c842380bdb2181c0fbde6f03fee724a9
QwesysDigitalSolution <qwesyssolutions@gmail.com>
2020-02-04 16:00:20 +0530

    KRS

--- lib/screens/Notification.dart
+++ lib/screens/Notification.dart
@@ -8,6 +8,8 @@ class NotificationScrren extends StatefulWidget {
 class _NotificationScrrenState extends State<NotificationScrren> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text("Notification"),
+    );
   }
 }
